d5f156a17864a2fe92b590fef3434dafd8d8280e
YYC <matt162162162@gmail.com>
2020-09-21 19:11:31 +0800

    Add bitfield.c

---
+++ bitfield.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include <stdio.h>
+bool is_one(int i) { return i == 1; }
+int main() {
+    struct {int a : 1; } obj = { .a = 1 };
+    puts(is_one(obj.a) ? "one" : "not one");
+    return 0;
+}
